a36674ef1bd1a34bee55e59dea10a2811989d0ba
billcoding <wangmiao07@sina.com>
2022-01-20 17:14:21 +0800

    Support m3u8

--- cmd/download.go
+++ cmd/download.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"github.com/billcoding/vip-video-downloader/channel"
-	"github.com/billcoding/vip-video-downloader/download"
 	m3u8DL "github.com/billcoding/vip-video-downloader/m3u8/download"
 	"github.com/spf13/cobra"
 	"os"
@@ -20,23 +19,27 @@ var (
 		Long: `Vip Video Download
 Supports: YouKu | QQ | SoHu TV | Mango TV ...
 If you need more, please contact me.`,
-		Example: `normal: vip-video-downloader download https://youku.com/v/xyz.html
-output dir: vip-video-downloader download https://youku.com/v/xyz.html -d /to/path
-output file: vip-video-downloader download https://youku.com/v/xyz.html -o my-video
-no convert: vip-video-downloader download https://youku.com/v/xyz.html -c=F
-convert to mkv format: vip-video-downloader download https://youku.com/v/xyz.html -f="mkv"
-special FFmpeg path: vip-video-downloader download https://youku.com/v/xyz.html -F="/to/path/ffmpeg"
-no use download channel: vip-video-downloader download https://example.com/index.m3u8 -U=F
-use download channel: vip-video-downloader download https://example.com/index.m3u8 -C=c1 -N=100`,
+		Example: `
+- normal: vip-video-downloader download "https://abc.com/v/xyz.html"
+- output dir: vip-video-downloader download -d "/to/path" "https://abc.com/v/xyz.html"
+- output file: vip-video-downloader download -o "my-video" "https://abc.com/v/xyz.html"
+- no convert(.ts): vip-video-downloader download -c=F "https://abc.com/v/xyz.html"
+- convert to mkv format: vip-video-downloader download -f="mkv" "https://abc.com/v/xyz.html"
+- special FFmpeg path: vip-video-downloader download -F="/to/path/ffmpeg" "https://abc.com/v/xyz.html"
+- m3u8 URL: vip-video-downloader download -m "https://abc.com/index.m3u8"
+- m3u8 File: vip-video-downloader download -m -M "/to/path/v.m3u8"`,
 		Run: downloadRun,
 	}
 
 	verbose    bool
 	keepTS     bool
+	tsDir      string
 	outputDir  string
 	outputFile string
 
-	useDownloadChannel  bool
+	m3u8     bool
+	m3u8File bool
+
 	downloadChannel     string
 	downloadConcurrency int
 
@@ -49,10 +52,13 @@ use download channel: vip-video-downloader download https://example.com/index.m3
 func init() {
 	downloadCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "V", false, "Print verbose log")
 	downloadCmd.PersistentFlags().BoolVarP(&keepTS, "keep", "k", false, "Keep TS files")
+	downloadCmd.PersistentFlags().StringVar(&tsDir, "ts-dir", "ts", "TS files directory")
 	downloadCmd.PersistentFlags().StringVarP(&outputDir, "output-dir", "d", "", "Output directory")
 	downloadCmd.PersistentFlags().StringVarP(&outputFile, "output-file", "o", "video", "Output file name without extension")
 
-	downloadCmd.PersistentFlags().BoolVarP(&useDownloadChannel, "use-download-channel", "U", true, "Use download channel")
+	downloadCmd.PersistentFlags().BoolVarP(&m3u8, "m3u8", "m", false, "m3u8 direct")
+	downloadCmd.PersistentFlags().BoolVarP(&m3u8File, "m3u8-file", "M", false, "m3u8 file")
+
 	downloadCmd.PersistentFlags().StringVarP(&downloadChannel, "download-channel", "C", "c1", "Download video channel: c1, ...")
 	downloadCmd.PersistentFlags().IntVarP(&downloadConcurrency, "download-concurrency", "N", 25, "Download video concurrency")
 
@@ -67,37 +73,26 @@ func downloadRun(_ *cobra.Command, args []string) {
 		panic("error: require URL")
 	}
 	URL := args[0]
-	URLExt := ""
-	isM3u8 := false
 	resultFile := ""
-	if useDownloadChannel {
+	if !m3u8 {
 		if c := channel.GetChannel(downloadChannel); c == nil {
 			panic("error: not support channel:" + downloadChannel)
 		} else {
-			URL, URLExt, isM3u8 = c.Parse(URL)
+			URL = c.Parse(URL)
+			if verbose {
+				fmt.Printf("[m3u8]%s\n", URL)
+			}
 		}
 	}
-
-	if isM3u8 {
-		tasker, err := m3u8DL.NewTask(outputDir, outputFile, URL, verbose)
-		if err != nil {
-			panic(err)
-		}
-		err = tasker.Start(downloadConcurrency)
-		if err != nil {
-			panic(err)
-		}
-		resultFile = tasker.OutputFile()
-	} else {
-		resultFile = filepath.Join(outputDir, outputFile+"."+URLExt)
-		dlER := download.Downloader{
-			URL:    URL,
-			Output: resultFile,
-		}
-		dlER.Start(verbose)
+	tasker, err := m3u8DL.NewTask(outputDir, outputFile, URL, tsDir, m3u8File, verbose)
+	if err != nil {
+		panic(err)
 	}
-
-	var err error
+	err = tasker.Start(downloadConcurrency)
+	if err != nil {
+		panic(err)
+	}
+	resultFile = tasker.OutputFile()
 	targetFilePath := filepath.Join(filepath.Dir(resultFile), outputFile+"."+strings.ToLower(convertFormat))
 	if convert && !strings.EqualFold(resultFile, targetFilePath) {
 		ffmpegCmd := exec.Command(ffmpegPath, "-y", "-i", resultFile, "-c", "copy", targetFilePath)
